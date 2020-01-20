2ea3fabe0414a27337082e55ce691bd0a009d6a4
wangce888 <1052220704@qq.com>
2020-01-20 17:22:04 +0800

    change filename

--- cv/__init__.py
+++ cv/__init__.py
@@ -1,4 +1,3 @@
 from pycontrol.cv.camera import *
 from pycontrol.cv.iproc import *
-from pycontrol.cv.stereo import *
-from pycontrol.cv.rgbd import *
+from pycontrol.cv.calib3d import *
--- cv/calib3d.py
+++ cv/calib3d.py
@@ -4,6 +4,39 @@ from pycontrol.matrix import lie
 
 
 
+def SGBM():
+    #TODO 完成
+    pass
+
+
+@nb.njit()
+def computeDepth(disparity, left_image, K, b):
+    """
+    根据视差计算深度
+    ---------------------
+    Calculate depth from parallax
+    """
+    fx, fy, cx, cy = K
+    h, w = disparity.shape[:2]
+    pointcloud = []
+    for v in range(h):
+        for u in range(w):
+            if disparity[v, u] <= 0 or disparity[v, u] >= 96.0:
+                continue
+
+            x = (u-cx)/fx
+            y = (v-cy)/fy
+            depth = fx*b/disparity[v, u]
+            x *= depth
+            y *= depth
+
+            # x, y, depth, color
+            point = [x, y, depth, left_image[v,u]/255.0]
+            pointcloud.append(point)
+
+    return pointcloud
+
+
 
 @nb.njit()
 def joint_map(colorImgs, depthImgs, poses, K, depthScale):
@@ -35,5 +68,7 @@ def joint_map(colorImgs, depthImgs, poses, K, depthScale):
                 pointcloud.append(pointWorld)
 
     return pointcloud
+
+
 
 
